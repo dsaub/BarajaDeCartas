e0f4bf21eea9d4d18e4e16cbc6721fe1da8040c4
Daniel <danilacasito8@gmail.com>
2024-11-26 12:53:35 +0100

    Remove javadoc from hist

--- package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"me.elordenador.barajadecartas"},{"l":"me.elordenador.barajadecartas.exceptions"},{"l":"me.elordenador.barajadecartas.tipos"}];updateSearchResults();
