04efa76fa80678b8dc58961e6da080a5eba96a9f
dsaub <54474838+dsaub@users.noreply.github.com>
2024-11-26 11:56:33 +0000

    Deploying to javadoc from @ dsaub/BarajaDeCartas@a4321b830ebe5c750d09ebb404879aa1abbb9644 🚀

---
+++ development/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"me.elordenador.barajadecartas"},{"l":"me.elordenador.barajadecartas.exceptions"},{"l":"me.elordenador.barajadecartas.tipos"}];updateSearchResults();
